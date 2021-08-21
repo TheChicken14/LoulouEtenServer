9a36e5983d492dc62e86ed7719ffec9be9da0db4
Wisse <wissehes@gmail.com>
2021-08-21 22:07:23 +0200

    Update checkAPIKey middleware to look for and check jwt

--- Middleware/checkApiKey.js
+++ Middleware/checkApiKey.js
@@ -1,11 +1,25 @@
+const User = require("../Models/User");
+const JWTManager = require("../Structures/JWTManager");
+
 /**
  *
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-module.exports = (req, res, next) => {
-  if (req.headers["authorization"] == process.env.API_URL) {
+module.exports = async (req, res, next) => {
+  if (req.path.startsWith("/auth/google")) {
+    return next();
+  }
+
+  const authHeader = req.headers["authorization"];
+  const decoded = JWTManager.decode(authHeader || null);
+  const user = decoded
+    ? await User.findOne({ googleId: decoded.googleId })
+    : null;
+
+  if (decoded && user) {
+    req.user = user;
     next();
   } else {
     res.status(403).json({
