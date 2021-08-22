2cb31f490659c9ad58117952f8f3f9ecbcff0c33
Wisse <wissehes@gmail.com>
2021-08-22 12:01:59 +0200

    Create pet model

---
+++ Models/Pet.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const Pet = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = model("pet", Pet);
