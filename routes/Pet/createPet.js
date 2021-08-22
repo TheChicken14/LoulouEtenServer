4a92e193a7b07adbffc508c9a2b00feaa5933b48
Wisse <wissehes@gmail.com>
2021-08-22 21:08:39 +0200

    Create createPet route

--- routes/Pet/createPet.js
+++ routes/Pet/createPet.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const Pet = require("../Models/Pet");
 
 const { body, validationResult } = require("express-validator");
+const prisma = require("../../Structures/Prisma");
 
 /* POST Create pet */
 router.post("/", body("name").isString(), async (req, res) => {
   const { name } = req.body;
+  const { id } = req.user;
 
   const result = validationResult(req);
   if (!result.isEmpty()) {
     return res.status(400).json({ errors: result.array() });
   }
 
-  const newPet = new Pet({
-    name,
+  const newPet = await prisma.pet.create({
+    data: {
+      name,
+      owners: {
+        connect: [{ id }],
+      },
+    },
   });
-  const savedPet = await newPet.save();
 
-  res.send(savedPet);
+  res.send(newPet);
 });
 
 module.exports = router;
