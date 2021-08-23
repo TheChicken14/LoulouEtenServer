311c0bff332346acf467b1a5044a882e9b2e505b
Wisse <wissehes@gmail.com>
2021-08-23 20:33:43 +0200

    Search for date as well

--- routes/Pet/feedPet.js
+++ routes/Pet/feedPet.js
@@ -44,11 +44,20 @@ router.post(
       });
     }
 
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0);
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59);
+
     const doesExist = await prisma.food.findFirst({
       where: {
         type: finalType,
         byId: userID,
         forId: pet.id,
+        date: {
+          gte: startOfDay,
+          lt: endOfDay,
+        },
       },
     });
 
