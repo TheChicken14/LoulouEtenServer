b4571367988182f9a33d28ad835e8ffb50017662
Wisse <wissehes@gmail.com>
2021-08-25 18:05:07 +0200

    use appleId

--- routes/authentication/index.js
+++ routes/authentication/index.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 const googleEndpoint = require("./googleEndpoint");
 const googleRedirect = require("./googleRedirect");
+const appleEndpoint = require("./appleEndpoint");
 const checkToken = require("./checkToken");
 
 router.use("/google/redirect", googleEndpoint);
 router.use("/google", googleRedirect);
+
+router.use("/apple", appleEndpoint);
+
 router.use("/test", checkToken);
 
 module.exports = router;
