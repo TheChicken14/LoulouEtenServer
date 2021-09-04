44da0d7eb0c2e3cc6e1596cf17724907ec11a2c3
Wisse <wissehes@gmail.com>
2021-09-04 15:34:13 +0200

    Add profile stuff to createPet

--- routes/Pet/createPet.js
+++ routes/Pet/createPet.js
@@ -5,25 +5,41 @@ const { body, validationResult } = require("express-validator");
 const prisma = require("../../Structures/Prisma");
 
 /* POST Create pet */
-router.post("/", body("name").isString(), async (req, res) => {
-  const { name } = req.body;
-  const { id } = req.user;
+router.post(
+  "/",
+  body("name").isString(),
+  body("birthdate").isISO8601(),
+  body("morningFood").isString(),
+  body("dinnerFood").isString(),
+  body("extraNotes").isString(),
+  async (req, res) => {
+    const { name, birthdate, morningFood, dinnerFood, extraNotes } = req.body;
+    const { id } = req.user;
 
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    return res.status(400).json({ errors: result.array() });
-  }
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
 
-  const newPet = await prisma.pet.create({
-    data: {
-      name,
-      owners: {
-        connect: [{ id }],
+    const newPet = await prisma.pet.create({
+      data: {
+        name,
+        owners: {
+          connect: [{ id }],
+        },
+        profile: {
+          create: {
+            birthday: birthdate,
+            morningFood: morningFood || "",
+            dinnerFood: dinnerFood || "",
+            extraNotes: extraNotes || "",
+          },
+        },
       },
-    },
-  });
+    });
 
-  res.send(newPet);
-});
+    res.send(newPet);
+  }
+);
 
 module.exports = router;
